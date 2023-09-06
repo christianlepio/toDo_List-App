f41d1efb62df1e45f0aff9189e5e878c6b3c7693
christianlepio <rylepio79@gmail.com>
2023-09-06 18:12:16 +0800

    add modal for action options

--- src/components/DisplayItems.jsx
+++ src/components/DisplayItems.jsx
@@ -34,7 +34,43 @@ const DisplayItems = (
             <li className={'list-group-item border-start-0 border-bottom-0 border-top-0 border-end-0 rounded text-break lh-lg ' + (index % 2 == 0 ? '' : 'bg-light')}>
                 {!isEdit && 
                     <span className="dropdown">
-                        <button className="btn btn-light btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" onClick={()=>setMiniHeight('25vh')} >
+                        {/* Button trigger modal */}
+                        <button type="button" className="btn btn-light btn-sm" data-bs-toggle="modal" data-bs-target="#modalAction">
+                            <i className="bi bi-three-dots-vertical"></i>
+                        </button>
+
+                        {/* Modal */}
+                        <div className="modal fade" id="modalAction" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div className="modal-dialog modal-dialog-centered">
+                                <div className="modal-content">
+                                    <div className="modal-header">{/*Modal title*/}
+                                        <h1 className="modal-title fs-5" id="exampleModalLabel">Options</h1>
+                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div className="modal-body">{/*Modal body*/}
+                                        <ul className="list-group list-group-flush">
+                                            <li className="list-group-item" data-bs-dismiss="modal" onClick={()=>{console.log(items.name); setIsEdit(true)}}>
+                                                <i className="bi bi-pencil-square text-warning"></i> Edit
+                                            </li>
+                                            <li className="list-group-item bg-light" data-bs-dismiss="modal" onClick={()=>removeGroceryItem(items.id)}>
+                                                <i className="bi bi-x-circle text-danger"></i> Delete
+                                            </li>
+                                            <li className="list-group-item" data-bs-dismiss="modal" onClick={()=>markAsDone(items.id, items.name)}>
+                                                <i className="bi bi-check-circle text-success"></i> Mark as Done
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div className="modal-footer">{/*Modal footer*/}
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+                        {/* <button className="btn btn-light btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" onClick={()=>setMiniHeight('25vh')} >
                             
                         </button>
                         <ul className="dropdown-menu">
@@ -53,7 +89,7 @@ const DisplayItems = (
                                 onClick={()=>markAsDone(items.id, items.name)}
                             >
                                 <i className="bi bi-check-circle text-success"></i> Mark as Done</li>
-                        </ul>
+                        </ul> */}
                     </span>
                 }
                 
