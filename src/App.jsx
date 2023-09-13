3c048ac463f4a587821e0f3d6943fe4253a8cc3a
christianlepio <rylepio79@gmail.com>
2023-09-14 01:18:29 +0800

    dark mode feature part3

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,10 @@ function App() {
       <div className="container">
         <GroceryContextProvider>
           <ToggleTheme />
-          <div style={{marginTop: '4vh',}}>
+          <div style={{marginTop: '3vh',}}>
             <div className="row justify-content-center">
               <div className="col-10 shadow rounded p-3 border-top border-info border-5 mb-5 todoBox">
-                <h1 className='fs-1 text-center font-monospace'>ToDo-List</h1>
+                <h1 className='fs-1 text-center font-monospace'><i className="fs-2 bi bi-view-list"></i><i className="fs-2 bi bi-align-top"></i><i className="fs-5 bi bi-app"></i>D<i className="fs-2 bi bi-app-indicator"></i>-Lis<i className="fs-2 bi bi-align-top"></i></h1>
                   <GroceryComponent />
                   <GroceryItemsComponent />
               </div>
